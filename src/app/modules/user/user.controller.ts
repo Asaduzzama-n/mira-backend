afd3b57e05f694380f721de5bbacff26271047bf
Asaduzzama-n <asad_uzzaman@outlook.com>
2025-09-16 02:28:53 +0600

    project setup done ready to roar

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,66 @@
+import { Request, Response, NextFunction } from 'express'
+
+import { StatusCodes } from 'http-status-codes'
+import catchAsync from '../../../shared/catchAsync'
+import sendResponse from '../../../shared/sendResponse'
+
+import { UserServices } from './user.service'
+import pick from '../../../shared/pick'
+import { userFilterableFields } from './user.constants'
+import { paginationFields } from '../../../interfaces/pagination'
+
+
+
+const updateProfile = catchAsync(async (req: Request, res: Response) => {
+  const { image, ...userData } = req.body
+
+  userData.profile = image[0]
+  const result = await UserServices.updateProfile(req.user!, userData)
+  sendResponse<String>(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Profile updated successfully',
+    data: result,
+  })
+})
+
+
+const checkAndGetUserNameAvailablity = catchAsync(async (req: Request, res: Response) => {
+  const { userName } = req.body
+  const result = await UserServices.checkAndGetUserNameAvailablity(userName)
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'User Name checked successfully',
+    data: result,
+  })
+})
+
+const getProfile = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserServices.getProfile(req.user!)
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Profile fetched successfully',
+    data: result,
+  })
+})
+
+const getAllUsers =catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query,userFilterableFields)
+  const pagination = pick(req.query,paginationFields)
+  const result = await UserServices.getAllUsers(req.user!,filters,pagination) 
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Users fetched successfully',
+    data: result,
+  })
+})
+
+export const UserController = {
+  checkAndGetUserNameAvailablity,
+  updateProfile,
+  getProfile,
+  getAllUsers
+}
